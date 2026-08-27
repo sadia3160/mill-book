b4d45781b7c1bfcb09442fbe386db3e081dfebb1
Sadia Sultana <sadias11.cse@gmail.com>
2026-08-27 23:03:41 +0600

    mongodb connection and server start

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,1 +1,16 @@
 //connects to mongoDB atlas
+
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
+        console.log(`MongoDB successfully connected ${connectionInstance.connection.host}`);
+    }
+    catch(err){
+        console.log(`MongoDB connection failed ${err}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,29 @@
 //starts the server
+
+import dotenv from "dotenv";
+import connectDB from "../config/database.js";
+import app from "./app.js";
+
+dotenv.config({
+    path: "./.env" 
+});
+
+const startServer = async () =>{
+    try{
+        await connectDB();
+
+        app.on("error", (err) => {      //whenever error event occurs in the application, it catches it
+            console.log(`ERROR ${err}`);
+            throw err;
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on PORT ${process.env.PORT}...`);
+        });
+    }
+    catch(err){
+        console.log(`MongoDB connection failed ${err}`);
+    }
+}
+
+startServer();
