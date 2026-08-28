1c0ac2ca5bb9477a8a1491f6e9d29d9a468a8431
Sadia Sultana <sadias11.cse@gmail.com>
2026-08-28 19:38:32 +0600

    restructure supplier info page

--- frontend/Supplier/supplier.js
+++ frontend/Supplier/supplier.js
@@ -1,101 +1,19 @@
-const API = "http://localhost:5000/api/sellers";
+const formDiv = document.querySelector(".form-container");
 
-const form = document.getElementById("sellerForm");
+const formBtn = document.querySelector("#new-supplier");
+const submit = document.querySelector("#formBtn-submit");
+const close = document.querySelector("#close");
 
-form.addEventListener("submit", async(e)=>{
-
-    e.preventDefault();
-
-    const quantity = Number(document.getElementById("quantity").value);
-    const price = Number(document.getElementById("pricePerKg").value);
-    const paid = Number(document.getElementById("paidAmount").value);
-
-    const total = quantity * price;
-    const due = total - paid;
-
-    const seller = {
-
-        sellerName: sellerName.value,
-        phone: phone.value,
-        address: address.value,
-        riceType: riceType.value,
-        quantity: quantity,
-        pricePerKg: price,
-        totalAmount: total,
-        paidAmount: paid,
-        dueAmount: due,
-        date: date.value
-
-    };
-
-    await fetch(API,{
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify(seller)
-    });
-
-    form.reset();
-
-    loadSellers();
-
+formBtn.addEventListener("click", () => {
+    formDiv.style.display = 'block';
 });
 
-async function loadSellers(){
+submit.addEventListener("click", () => {
+    formDiv.style.display = none;
+});
 
-    const response = await fetch(API);
-    const sellers = await response.json();
+close.addEventListener("click", () => {
+    formDiv.style.display = none;
+});
 
-    const table = document.getElementById("sellerTable");
 
-    table.innerHTML="";
-
-    sellers.forEach((seller)=>{
-
-        table.innerHTML +=`
-
-        <tr>
-
-        <td>${seller.sellerName}</td>
-
-        <td>${seller.phone}</td>
-
-        <td>${seller.riceType}</td>
-
-        <td>${seller.totalAmount}</td>
-
-        <td>${seller.paidAmount}</td>
-
-        <td>${seller.dueAmount}</td>
-
-        <td>
-
-        <button class="deleteBtn"
-        onclick="deleteSeller('${seller._id}')">
-
-        Delete
-
-        </button>
-
-        </td>
-
-        </tr>
-
-        `;
-
-    });
-
-}
-
-async function deleteSeller(id){
-
-    await fetch(API+"/"+id,{
-        method:"DELETE"
-    });
-
-    loadSellers();
-
-}
-
-loadSellers();
